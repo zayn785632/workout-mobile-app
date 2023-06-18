ac2ffb9627a5de5758449476761f340b7f25f33d
Mostafa <md.mostafa11@icloud.com>
2023-06-19 01:24:03 +0600

    new commit

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,11 +1,11 @@
 
 import { useEffect } from "react";
 import { View, Text, Button } from "react-native";
+import { NativeStackHeaderProps } from "@react-navigation/native-stack/lib/typescript/src/types";
 
 
 
-
-export default function HomeScreen({navigation}:any) {
+export default function HomeScreen({navigation}: NativeStackHeaderProps) {
 
     useEffect(() =>{
         console.log("Rendering Home Screen");
--- screens/PlannerScreen.tsx
+++ screens/PlannerScreen.tsx
@@ -1,8 +1,9 @@
 
 import { useEffect } from "react";
 import { View, Text, Button } from "react-native";
+import { NativeStackHeaderProps } from "@react-navigation/native-stack/lib/typescript/src/types";
 
-export default function PlannerScreen({navigation}:any) {
+export default function PlannerScreen({navigation}: NativeStackHeaderProps) {
     
     useEffect(() =>{
         console.log("Rendering Planner Screen");
