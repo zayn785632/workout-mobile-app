15212c141e8e8eabd4539d2e5053ac289181b825
Mostafa <md.mostafa11@icloud.com>
2023-06-19 22:22:05 +0600

    added new files

--- App.tsx
+++ App.tsx
@@ -1,7 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import Navigation from './navigation';
+import useCachedResources from './hooks/useCachedResources';
 
 export default function App() {
+  const isLoaded = useCachedResources();
+  console.log(isLoaded);
   return (
     <>
       <Navigation/>
---
+++ hooks/useCachedResources.ts
@@ -0,0 +1,27 @@
+
+import { useEffect, useState } from "react";
+import * as Font from "expo-font";
+
+export default function useCachedResources(){
+    const [isLoadingComplete, setIsLoadingComplete] = useState(false);
+
+    useEffect(() => {
+        
+       async function loadResourcesAndDataAsync(){
+           try{
+            await Font.loadAsync({
+                "montserrat": require("../assets/fonts/Montserrat-Regular.ttf"),
+                "montserrat-bold": require("../assets/fonts/Montserrat-Bold.ttf"),
+            })
+           } catch (e){
+            console.warn(e);
+           } finally {
+            setIsLoadingComplete(true);
+           }
+        }
+        loadResourcesAndDataAsync();
+    }, [])
+
+    
+    return isLoadingComplete;
+}
